e56d068151435ac7314d903c1d4696e14433bf9b
Mark Kelsall <mark.kelsall@gmail.com>
2018-02-07 22:54:09 +0000

    change NoResultItem to pure function

--- src/components/NoResultsItem/NoResultsItem.js
+++ src/components/NoResultsItem/NoResultsItem.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import './NoResultsItem.css';
 
-export default class NoResultsItem extends Component {
-
-    render = () => {
-        return (
-            <li className="ui-menu-item" role="menuitem" tabIndex="-1">
-                <a className="ui-state-disabled">
-                    <span>No results found</span>
-                </a>
-            </li>
-        );
-    }
+export default () => {
+    return (
+        <li className="ui-menu-item" role="menuitem" tabIndex="-1">
+            <a className="ui-state-disabled">
+                <span>No results found</span>
+            </a>
+        </li>
+    );
 }
